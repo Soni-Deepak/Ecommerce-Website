0da09d03881fd71e9439e4d0243325a41c5f770d
Soni-Deepak <deepaksoni00450420@gmail.com>
2026-09-08 13:00:35 +0530

    Added Home page

--- Ecommerce/src/App.jsx
+++ Ecommerce/src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Auth from './pages/Auth'
+import Checkout from './pages/Checkout'
+import Navbar from './components/Navbar'
+import "./App.css"
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='app' >
+      <Navbar />
+      <Routes>
+        <Route path='/' element = {<Home/> } />
+        <Route path='/auth' element = {<Auth /> }  />
+        <Route path='/checkout' element = {<Checkout /> }  />
+      </Routes>
     </div>
   )
 }
