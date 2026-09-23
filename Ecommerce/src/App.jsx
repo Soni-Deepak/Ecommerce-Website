785f19dbef5e30d2af364a4c536ee24b8574947e
Soni-Deepak <deepaksoni00450420@gmail.com>
2026-09-23 12:44:16 +0530

    Finished Cart And Checkout

--- Ecommerce/src/App.jsx
+++ Ecommerce/src/App.jsx
@@ -7,10 +7,12 @@ import Navbar from './components/Navbar'
 import "./App.css"
 import AuthProvider from './context/AuthContext'
 import ProductDetails from "./pages/ProductDetails";
+import CartProvider from './context/CartContext'
 
 const App = () => {
   return (
     <AuthProvider>
+      <CartProvider>
     <div className='app' >
       <Navbar />
       <Routes>
@@ -20,6 +22,7 @@ const App = () => {
         <Route path='/products/:id' element = {<ProductDetails /> }  />
       </Routes>
     </div>
+    </CartProvider>
     </AuthProvider>
   )
 }
--- Ecommerce/src/context/AuthContext.jsx
+++ Ecommerce/src/context/AuthContext.jsx
@@ -1,6 +1,6 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState , useContext } from "react";
 
-export const AuthContext = createContext(null);
+ const AuthContext = createContext(null);
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(
@@ -53,5 +53,11 @@ const AuthProvider = ({ children }) => {
     </AuthContext.Provider>
   );
 };
+
+export function useAuth(){
+  const context = useContext(AuthContext);
+
+  return context;
+}
 
 export default AuthProvider;
